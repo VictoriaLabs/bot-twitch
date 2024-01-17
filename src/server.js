2053416f453fe19f34c9a5e4d35f21f5951430c1
LoanF <loanfrancois1@gmail.com>
2024-01-17 15:02:21 +0100

    feat: poll + commands folder

--- src/server.js
+++ src/server.js
@@ -2,28 +2,34 @@ require('dotenv').config();
 
 const tmi = require('tmi.js');
 const socket = require('./socket.js');
+const fs = require('fs');
+const path = require('path');
+const commands = {};
 
 // Expression régulière utilisée pour analyser les commandes de chat.
 const regexpCommand = new RegExp(/^!([a-zA-Z0-9]+)(?:\W+)?(.*)?/);
 
-const commands = {
-    ping: {
-        response: 'Pong !'
-    }
-}
-
 const client = new tmi.Client({
-	options: { debug: true },
-	identity: {
-		username: process.env.TWITCH_BOT_USERNAME,
-		password: process.env.TWITCH_OAUTH_TOKEN
-	},
-    
-	channels: [ 'victorialabs' ]
+    options: { debug: true },
+    identity: {
+        username: process.env.TWITCH_BOT_USERNAME,
+        password: process.env.TWITCH_OAUTH_TOKEN
+    },
+    channels: ['victorialabs']
 });
 
 client.connect().catch((err) => {
     console.error('Failed to connect :', err);
+});
+
+client.on('connected', () => {
+    const commandFiles = fs.readdirSync(path.join(__dirname, '/commands')).filter(file => file.endsWith('.js'));
+
+    for (const file of commandFiles) {
+        const command = require(`./commands/${file}`);
+        commands[command.name] = command;
+        console.log(`Commande chargée : ${command.name}`)
+    }
 });
 
 client.on('message', (channel, tags, message, self) => {
@@ -37,12 +43,20 @@ client.on('message', (channel, tags, message, self) => {
     if (commandMatch) {
         const [raw, command, argument] = commandMatch;
 
+
+
         const { response } = commands[command] || {};
 
         if (typeof response === 'function') {
-            client.say(channel, response(tags.username, argument));
-        } else if (typeof response === 'string') {
-            client.say(channel, response);
+            response(client, tags.username, argument).then(result => {
+                if (typeof result === 'string') {
+                    client.say(channel, result);
+                } else {
+                    console.log('Erreur : la réponse de la fonction n\'est pas une chaîne.');
+                }
+            }).catch(error => {
+                console.log('Erreur : ', error);
+            });
         }
     } else {
         // Si le message n'est pas une commande, on l'envoie au WebSocket
