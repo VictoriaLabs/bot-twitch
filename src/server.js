4f92a729d053fa7a0cc48c2fe452dd6467126307
Loan <96796739+LoanF@users.noreply.github.com>
2024-04-09 16:37:34 +0200

    feat/unification tchat

--- src/server.js
+++ src/server.js
@@ -1,31 +1,43 @@
 require('dotenv').config();
 
+const Sentry = require("@sentry/node");
 const tmi = require('tmi.js');
-const twitchAPI = require('./twitchAPI.js');
 const socket = require('./socket.js');
+const fs = require('fs');
+const path = require('path');
+const commands = {};
 
+Sentry.init({ dsn: process.env.SENTRY_DSN });
 
 // Expression régulière utilisée pour analyser les commandes de chat.
 const regexpCommand = new RegExp(/^!([a-zA-Z0-9]+)(?:\W+)?(.*)?/);
 
-const commands = {
-    ping: {
-        response: 'Pong !'
-    }
-}
-
 const client = new tmi.Client({
-	options: { debug: true },
-	identity: {
-		username: process.env.TWITCH_BOT_USERNAME,
-		password: process.env.TWITCH_OAUTH_TOKEN
-	},
-    
-	channels: [ 'victorialabs' ]
+    options: { debug: true },
+    identity: {
+        username: process.env.TWITCH_BOT_USERNAME,
+        password: process.env.TWITCH_OAUTH_TOKEN
+    },
+    channels: ['avamind']
 });
 
 client.connect().catch((err) => {
     console.error('Failed to connect :', err);
+});
+
+client.on('connected', () => {
+    const commandFiles = fs.readdirSync(path.join(__dirname, '/commands')).filter(file => file.endsWith('.js'));
+
+    for (const file of commandFiles) {
+        const command = require(`./commands/${file}`);
+        commands[command.name] = command;
+        console.log(`Commande chargée : ${command.name}`)
+    }
+
+    client.on('stream-up', (channel, tags) => {
+        console.log(`Stream by ${tags['display-name']} just went live!`);
+        socket.emitEvent('streamLive', channel);
+    });
 });
 
 client.on('message', (channel, tags, message, self) => {
@@ -42,16 +54,36 @@ client.on('message', (channel, tags, message, self) => {
         const { response } = commands[command] || {};
 
         if (typeof response === 'function') {
-            client.say(channel, response(tags.username, argument));
-        } else if (typeof response === 'string') {
-            client.say(channel, response);
+            response(client, tags.username, argument).then(result => {
+                if (typeof result === 'string') {
+                    console.log(result);
+                    client.say(channel, result);
+                } else {
+                    console.log('Erreur : la réponse de la fonction n\'est pas une chaîne.');
+                }
+            }).catch(error => {
+                console.log('Erreur : ', error);
+            });
         }
     } else {
         // Si le message n'est pas une commande, on l'envoie au WebSocket
         try {
-            socket.emitEvent('twitch', { channel, tags, message });
+            const messageJSON = {
+                platform: 'twitch',
+                channelName: channel,
+                username: tags.username,
+                message,
+            };
+            socket.emitEvent('sendMessage', messageJSON);
         } catch (error) {
             console.log("Erreur lors de l'émission d'un message au WebSocket : ", error);
         }
     }
+});
+
+socket.onEvent('receiveMessage', (message) => {
+    // if (message.platform === 'twitch') {
+    //     client.say(message.channelName, message.message);
+    // }
+    console.log(message);
 });
--- src/twitchAPI.js
+++ src/twitchAPI.js
@@ -1,16 +1,19 @@
 const twitchAPI = {
   baseURL: process.env.TWITCH_API_URL,
   clientID: process.env.TWITCH_CLIENT_ID,
-  fetchWithHeaders: async function (url) {
+  fetchWithHeaders: async function (url, method, body) {
     const response = await fetch(url, {
-      headers: {
-        "Client-ID": this.clientID,
-        "Authorization": `Bearer ${process.env.TWITCH_OAUTH_TOKEN}`
-      }
+        method: method || 'GET',
+        headers: {
+            "Client-ID": this.clientID,
+            "Authorization": `Bearer ${process.env.TWITCH_OAUTH_TOKEN}`,
+            "Content-Type": "application/json"
+        },
+        body: body ? JSON.stringify(body) : undefined
     });
     const data = await response.json();
     return data;
-  },
+},
   getUser: async function (username) {
     const url = `${this.baseURL}/users?login=${username}`;
     return this.fetchWithHeaders(url);
@@ -19,13 +22,23 @@ const twitchAPI = {
     const url = `${this.baseURL}/channels/followers?broadcaster_id=${userID}`;
     return this.fetchWithHeaders(url);
   },
-  getChannelInfo: async (broadcastID) => {
+  getChannelInfo: async function (broadcastID) {
     const url = `${this.baseURL}/channels?broadcaster_id=${broadcastID}`;
     return this.fetchWithHeaders(url);
   },
   getBitsLeaderboard: async () => {
     const url = `${this.baseURL}/bits/leaderboard`;
     return this.fetchWithHeaders(url);
+  },
+  createPoll: async function (broadcastID, question, options, duration) {
+    const url = `${this.baseURL}/polls`;
+    const body = {
+      broadcaster_id: broadcastID,
+      title: question,
+      choices: options.map(choice => ({ title: choice })),
+      duration: duration || 60
+    };
+    return this.fetchWithHeaders(url, 'POST', body);
   }
 };
 
