e70cd9eaaa6bc69076ae6d9886a2854ec4eecc9a
kaymeric <aymeric.kreuzer@ynov.com>
2024-05-21 23:40:34 +0200

    feat : chat replication, send message into chat and fixbug

--- src/server.js
+++ src/server.js
@@ -12,43 +12,66 @@ const commands = {
     }
 }
 
-const client = new tmi.Client({
-	options: { debug: true },
-	identity: {
-		username: process.env.TWITCH_BOT_USERNAME,
-		password: process.env.TWITCH_OAUTH_TOKEN
-	},
+let client;
+
+socket.onEvent('start', async (data) => {
+    if (!data.twitch) return;
+
+    client = new tmi.Client({
+        options: { debug: true },
+        identity: {
+            username: process.env.TWITCH_BOT_USERNAME,
+            password: process.env.TWITCH_OAUTH_TOKEN
+        },
+        
+        channels: [ data.twitch ]
+    });
     
-	channels: [ 'victorialabs' ]
+    client.connect().catch((err) => {
+        console.error('Failed to connect :', err);
+    });
+    
+    client.on('message', (channel, tags, message, self) => {
+        if (!tags.username) return;
+        if (tags.username.toLowerCase() == process.env.TWITCH_BOT_USERNAME.toLowerCase()) return;
+    
+        const commandMatch = message.match(regexpCommand);
+        
+        // Vérifier si le message est une commande
+        if (commandMatch) {
+            const [raw, command, argument] = commandMatch;
+    
+            const { response } = commands[command] || {};
+    
+            if (typeof response === 'function') {
+                client.say(channel, response(tags.username, argument));
+            } else if (typeof response === 'string') {
+                client.say(channel, response);
+            }
+        } else {   
+            try {
+                const data = {
+                    platform: "twitch",
+                    channel: channel.slice(1),
+                    username: tags.username,
+                    message: message,
+                  };
+                socket.emitEvent('sendMessage', data);
+            } catch (error) {
+                console.log("Erreur lors de l'émission d'un message au WebSocket : ", error);
+            }
+        }
+    });
+    
+    socket.onEvent('receiveMessage', (message) => {
+        // client.say(message.channel, "["+message.username+"]" +message.message);
+        client.say("victorialabs", "["+message.username+"] : " +message.message);
+    });
 });
 
-client.connect().catch((err) => {
-    console.error('Failed to connect :', err);
-});
+socket.onEvent('stop', (data) => {
+    if (!client || !data.twitch) return;
 
-client.on('message', (channel, tags, message, self) => {
-    const isNotBot = tags.username.toLowerCase() !== process.env.TWITCH_BOT_USERNAME;
-
-    if (!isNotBot) return;
-
-    const commandMatch = message.match(regexpCommand);
-
-    // Vérifier si le message est une commande
-    if (commandMatch) {
-        const [raw, command, argument] = commandMatch;
-
-        const { response } = commands[command] || {};
-
-        if (typeof response === 'function') {
-            client.say(channel, response(tags.username, argument));
-        } else if (typeof response === 'string') {
-            client.say(channel, response);
-        }
-    } else {
-        try {
-            socket.emitEvent('message', { channel, tags, message });
-        } catch (error) {
-            console.log("Erreur lors de l'émission d'un message au WebSocket : ", error);
-        }
-    }
+    client.disconnect();
+    socket.offEvent('receiveMessage');
 });
--- src/socket.js
+++ src/socket.js
@@ -2,20 +2,30 @@ require('dotenv').config();
 
 const socket = require('socket.io-client')(process.env.WEBSOCKET_URL);
 
+
 function emitEvent(eventName, data) {
-    if (socket.connected)
-      socket.emit(eventName, data);
-    else
-      console.log('Le socket n\'est pas connecté.');
+  if (socket.connected)
+    socket.emit(eventName, data);
+  else
+  console.log('Le socket n\'est pas connecté.');
 };
 
 function onEvent(eventName, callback) {
-    socket.on(eventName, callback);
+  socket.on(eventName, callback);
+};
+
+function offEvent(eventName) {
+  socket.off(eventName);
 };
 
 function disconnect() {
-    socket.disconnect();
-    console.log('Déconnecté du serveur WebSocket');
+  socket.disconnect();
+  console.log('Déconnecté du serveur WebSocket');
 };
 
-module.exports = { emitEvent, onEvent, disconnect };
+onEvent('connect', () => {
+  console.log('Connecté au serveur WebSocket')
+  socket.emit('join', "victoria")
+});
+
+module.exports = { socket, emitEvent, onEvent, offEvent, disconnect };
