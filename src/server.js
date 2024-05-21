c895b8acb4d6b1ab56d04d9d9e8a7a3e13cae5e0
KAymeric <115625523+KAymeric@users.noreply.github.com>
2024-05-22 00:20:56 +0200

    Feature/chat replication (#13)
    Co-authored-by: LoanF <loanfrancois1@gmail.com>
    Co-authored-by: Loan <96796739+LoanF@users.noreply.github.com>

--- src/server.js
+++ src/server.js
@@ -5,80 +5,91 @@ const tmi = require('tmi.js');
 const socket = require('./socket.js');
 const fs = require('fs');
 const path = require('path');
-const commands = {};
+const twitchAPI = require('./twitchAPI.js');
 
 Sentry.init({ dsn: process.env.SENTRY_DSN });
 
 // Expression régulière utilisée pour analyser les commandes de chat.
 const regexpCommand = new RegExp(/^!([a-zA-Z0-9]+)(?:\W+)?(.*)?/);
 
-const client = new tmi.Client({
-    options: { debug: true },
-    identity: {
-        username: process.env.TWITCH_BOT_USERNAME,
-        password: process.env.TWITCH_OAUTH_TOKEN
-    },
-    channels: ['avamind']
-});
+let client;
 
-client.connect().catch((err) => {
-    console.error('Failed to connect :', err);
-});
+socket.onEvent('start', async (data) => {
+    if (!data.twitch) return;
 
-client.on('connected', () => {
-    const commandFiles = fs.readdirSync(path.join(__dirname, '/commands')).filter(file => file.endsWith('.js'));
+    client = new tmi.Client({
+        options: { debug: true },
+        identity: {
+            username: process.env.TWITCH_BOT_USERNAME,
+            password: process.env.TWITCH_OAUTH_TOKEN
+        },
+        
+        channels: [ data.twitch ]
+    });
+    
+    client.connect().catch((err) => {
+        console.error('Failed to connect :', err);
+    });
+    
+    client.on('message', (channel, tags, message, self) => {
+        if (!tags.username) return;
+        if (tags.username.toLowerCase() == process.env.TWITCH_BOT_USERNAME.toLowerCase()) return;
+    
+        const commandMatch = message.match(regexpCommand);
+        
+        // Vérifier si le message est une commande
+        if (commandMatch) {
+            const [raw, command, argument] = commandMatch;
+    
+            const { response } = commands[command] || {};
+    
+            if (typeof response === 'function') {
+                client.say(channel, response(tags.username, argument));
+            } else if (typeof response === 'string') {
+                client.say(channel, response);
+            }
+        } else {   
+            try {
+                const data = {
+                    platform: "twitch",
+                    channel: channel.slice(1),
+                    username: tags.username,
+                    message: message,
+                  };
+                socket.emitEvent('sendMessage', data);
+            } catch (error) {
+                console.log("Erreur lors de l'émission d'un message au WebSocket : ", error);
+            }
+        }
+    });
+    
+    socket.onEvent('receiveMessage', (message) => {
+        // client.say(message.channel, "["+message.username+"]" +message.message);
+        client.say("victorialabs", "["+message.username+"] : " +message.message);
+    });
 
-    for (const file of commandFiles) {
-        const command = require(`./commands/${file}`);
-        commands[command.name] = command;
-        console.log(`Commande chargée : ${command.name}`)
-    }
+    
+    client.on('connected', () => {
+        const commandFiles = fs.readdirSync(path.join(__dirname, '/commands')).filter(file => file.endsWith('.js'));
 
-    client.on('stream-up', (channel, tags) => {
-        console.log(`Stream by ${tags['display-name']} just went live!`);
-        socket.emitEvent('streamLive', channel);
+        for (const file of commandFiles) {
+            const command = require(`./commands/${file}`);
+            commands[command.name] = command;
+            console.log(`Commande chargée : ${command.name}`)
+        }
+
+        client.on('stream-up', (channel, tags) => {
+            console.log(`Stream by ${tags['display-name']} just went live!`);
+            socket.emitEvent('streamLive', channel);
+        });
     });
 });
 
-client.on('message', (channel, tags, message, self) => {
-    const isNotBot = tags.username.toLowerCase() !== process.env.TWITCH_BOT_USERNAME;
+socket.onEvent('stop', (data) => {
+    if (!client || !data.twitch) return;
 
-    if (!isNotBot) return;
-
-    const commandMatch = message.match(regexpCommand);
-
-    // Vérifier si le message est une commande
-    if (commandMatch) {
-        const [raw, command, argument] = commandMatch;
-
-        const { response } = commands[command] || {};
-
-        if (typeof response === 'function') {
-            response(client, tags.username, argument).then(result => {
-                if (typeof result === 'string') {
-                    console.log(result);
-                    client.say(channel, result);
-                } else {
-                    console.log('Erreur : la réponse de la fonction n\'est pas une chaîne.');
-                }
-            }).catch(error => {
-                console.log('Erreur : ', error);
-            });
-        }
-    } else {
-        // Si le message n'est pas une commande, on l'envoie au WebSocket
-        try {
-            const messageJSON = {
-                platform: 'twitch',
-                channelName: channel,
-                username: tags.username,
-                message,
-            };
-            socket.emitEvent('sendMessage', messageJSON);
-        } catch (error) {
-            console.log("Erreur lors de l'émission d'un message au WebSocket : ", error);
-        }
-    }
+    client.disconnect();
+    socket.offEvent('receiveMessage');
 });
 
 socket.onEvent('receiveMessage', (message) => {
